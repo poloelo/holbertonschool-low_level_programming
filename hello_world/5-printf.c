e9b9444c3b8026ca6fa7221e27cf2d4f69d4cf00
poloelo <p.gioria2304@gmail.com>
2025-10-20 12:30:46 +0200

    Add the 5th prog

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - use printf function to print hatever is in the function
+ * Return: returns 0 if everything went well
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
