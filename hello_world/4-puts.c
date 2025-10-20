0a5d6821201b5fcef31fcbf35c814ccc0f3339b1
poloelo <p.gioria2304@gmail.com>
2025-10-20 12:17:34 +0200

    rectif

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/*
+ * The main function prints the value wanted
+**/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
 }
