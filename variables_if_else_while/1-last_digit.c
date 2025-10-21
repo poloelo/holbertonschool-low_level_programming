8605071cf168f39635431d57213af2e8e418c0dc
poloelo <p.gioria2304@gmail.com>
2025-10-21 02:15:24 +0200

    rectif

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,23 +9,24 @@
 
 int main(void)
 {
+int l_digit;
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int last_digit = n % 10;
-printf("Last digit of %i is %i", n, last_digit);
-if (last_digit != 0)
+l_digit = n % 10;
+printf("Last digit of %i is %i", n, l_digit);
+if (l_digit != 0)
 {
-if (last_digit < 6)
+if (l_digit < 6)
 {
 printf(" and is less than 6");
 }
-else if (last_digit > 5)
+else if (l_digit > 5)
 {
 printf(" and is greater than 5");
 }
 }
-if (last_digit == 0)
+if (l_digit == 0)
 {
 printf(" and is 0\n");
 }
