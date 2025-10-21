782bb202d43caff7062212172874950e81998346
poloelo <p.gioria2304@gmail.com>
2025-10-21 02:47:16 +0200

    Add the 5th prog

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in 3 lines
+ * Return: 0 if everything went great
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i = i + 1)
+printf("%d", i);
+putchar('\n');
+return (0);
+}
