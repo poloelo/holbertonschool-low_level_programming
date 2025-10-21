6124007316deff3e15a803efd4338d8668acb380
poloelo <p.gioria2304@gmail.com>
2025-10-21 02:50:03 +0200

    Add the 6th prog

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in 3 lines
+ * Return: 0 if everything went great
+ */
+
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i = i + 1)
+putchar(i);
+putchar('\n');
+return (0);
+}
