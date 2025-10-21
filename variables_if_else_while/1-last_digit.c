e5dad79c94b8958fe3293834593e04beaccd7100
poloelo <p.gioria2304@gmail.com>
2025-10-21 02:05:13 +0200

    Add the 1st prog

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Verifiy via conditions the number randomly choosen
+* Return: 0 if all is working
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+int last_digit = n % 10;
+printf("Last digit of %i is %i", n, last_digit);
+if (last_digit != 0)
+{
+if (last_digit < 6)
+{
+printf(" and is less than 6");
+}
+else if (last_digit > 5)
+{
+printf(" and is greater than 5");
+}
+}
+if (last_digit == 0)
+{
+printf(" and is 0\n");
+}
+else
+{
+printf(" and is not 0\n");
+}
+return (0);
+}
