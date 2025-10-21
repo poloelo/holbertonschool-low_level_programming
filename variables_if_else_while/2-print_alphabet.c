27161a215ee9dc52bff850d5421bb16ca5bed07d
poloelo <p.gioria2304@gmail.com>
2025-10-21 02:31:00 +0200

    Add the 2nd prog

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in 3 lines
+ * Return: 0 if everything went great
+ */
+
+int main(void)
+{
+for (int i = 97; i <= 122; i = i + 1)
+putchar(i);
+putchar('\n');
+return (0);
+}
