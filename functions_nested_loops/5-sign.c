83a145adb56003b400206e852032e67a3342d9f8
poloelo <p.gioria2304@gmail.com>
2025-10-23 14:45:43 +0200

    add the 5th prog

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_sign - definition of the func
+ * @n: number
+ */
+
+
+
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+else if (n == 0)
+return (0);
+else
+return (-1);
+}
