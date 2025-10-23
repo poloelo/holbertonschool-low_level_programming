58b54c3fa77fadea2ab413e2541f562a90a6e09b
poloelo <p.gioria2304@gmail.com>
2025-10-23 14:00:19 +0200

    rect

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- *
  *print_alphabet_x10 - def
- *
  */
-
 
 void print_alphabet_x10(void)
 {
-    int i;
-    for (i = 0; i <= 11;i=i+1)
-    {
-    print_alphabet();
-    }
+int i;
+for (i = 0 ; i <= 11 ; i = i + 1)
+{
+print_alphabet();
+}
 }
