bb9dc6d7ef174f6df3cedb3b40bdec7cea3d25b1
poloelo <p.gioria2304@gmail.com>
2025-10-23 13:11:41 +0200

    Add the dir nested loops

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - The programm prints the string "_putchar" with putchar function
+ *
+ * Return: 0 if everything went well
+ *
+ */
+
+int main(void)
+{
+putchar(95);
+putchar(112);
+putchar(117);
+putchar(116);
+putchar(99);
+putchar(104);
+putchar(97);
+putchar(114);
+putchar('\n');
+return (0);
+}
