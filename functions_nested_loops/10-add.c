487ea85f8824cbac183712b2af04b30e736eabf5
poloelo <p.gioria2304@gmail.com>
2025-10-24 12:47:44 +0200

    Add the 10th prog

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include main.h
+
+/**
+ * add - adds two integers and returns the result
+ * @n: first integer
+ * @m: second integer
+ *
+ * Return: the sum of n and m
+ */
+int add(int n, int m)
+{
+	int result;
+
+	result = n + m;
+
+	return (result);
+}
