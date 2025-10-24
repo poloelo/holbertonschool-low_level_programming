ef4548fc1d37a75c6c26c3749c3b960ad04faa6c
poloelo <p.gioria2304@gmail.com>
2025-10-24 11:40:24 +0200

    add the 7th prog

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ *@n: def of a paramrtre
+ *Return: 0 if everything goes wrong
+ */
+int print_last_digit(int n)
+{
+return (n % 10);
+}
