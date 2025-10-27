43d2adb48d7b234bf56b0156fcf5fe7d515bc5b0
poloelo <p.gioria2304@gmail.com>
2025-10-27 11:02:19 +0100

    rectif

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,16 @@
 #include "main.h"
-
+/**
+ * _isupper - function description
+ *
+ * @c : a value
+ *
+ * Return: a value
+ *
+ */
 int _isupper(int c)
 {
-    if(c >= 65 && c <= 90) return (1);
-    else return (0);
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
 }
