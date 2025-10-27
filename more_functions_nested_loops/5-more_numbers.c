e4f575496eeeaceb125e14e75ea85a458d2320b0
poloelo <p.gioria2304@gmail.com>
2025-10-27 12:15:45 +0100

    Add the 5th prog

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - function
+ */
+
+void more_numbers(void)
+{
+int i;
+int j:
+for (i = 0; i <= 14; i++)
+{
+if (i >= 10)
+{
+_putchar('0' + 1);
+}
+_putchar('0' + (i % 10));
+}
+}
