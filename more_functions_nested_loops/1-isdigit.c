4b10512016d787236fbd268fbbb26051bcd995ec
poloelo <p.gioria2304@gmail.com>
2025-10-27 11:05:52 +0100

    Add the 1st prog

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - function description
+ *
+ * @c : a value
+ *
+ * Return: a value
+ *
+ */
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+return (1);
+else
+return (0);
+}
