811f3aca5d75454d92707216d23e92dfab3ec618
poloelo <p.gioria2304@gmail.com>
2025-10-27 12:23:16 +0100

    rectif

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,6 +7,9 @@
 void more_numbers(void)
 {
 int i;
+int j;
+for (j = 0, j <= 10; j++)
+{
 for (i = 0; i <= 14; i++)
 {
 if (i >= 10)
@@ -16,4 +19,5 @@ _putchar('0' + 1);
 _putchar('0' + (i % 10));
 }
 _putchar('\n');
+}
 }
