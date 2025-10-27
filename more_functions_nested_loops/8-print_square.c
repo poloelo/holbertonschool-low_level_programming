c608224c7233401969565e1cd4de5bb4fb4652a8
poloelo <p.gioria2304@gmail.com>
2025-10-27 13:07:11 +0100

    rect

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -4,17 +4,12 @@ void print_square(int size)
 {
 int x;
 int y;
-if (size <= 0)
-{
-_putchar('\n');
-} 
-
-for (x = 0; x < size; x++)
-    
+for (x = 0; y < size; x++)
+{    
     for(y = 0; y < size; y++)
     {
         _putchar('#');
     }
-
     _putchar('\n');
+}
 }
