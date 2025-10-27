446fef0db84931cad6f9ffefdbf855c4eb177500
poloelo <p.gioria2304@gmail.com>
2025-10-27 11:31:59 +0100

    Add the 4th prog

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_most_numbers - Prints all number except 2 and 4
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+_putchar('0' + i);
+}
+
+}
