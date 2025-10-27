1a38acfdce0743d64beeee068767ca1e2e9224d7
poloelo <p.gioria2304@gmail.com>
2025-10-27 11:25:43 +0100

    rectif

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,11 @@
  */
 void print_numbers(void)
 {
-    int i;
+int i;
 
-    for (i = 0; i <= 9; i++)
-        _putchar(i + '0');  /* 1re utilisation */
+for (i = 0; i <= 9; i++)
+_putchar(i + '0');
 
-    _putchar('\n');          /* 2e utilisation */
+_putchar('\n');      
 }
 
