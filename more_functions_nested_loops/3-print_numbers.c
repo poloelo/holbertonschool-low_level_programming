2da09bb8d92b9b125e91faf14676ab8b2ec247c9
poloelo <p.gioria2304@gmail.com>
2025-10-27 11:21:30 +0100

    rectif

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * print_numbers - a function
- * Return: 0
+ * print_numbers - prints numbers from 0 to 9
+ * Return: nothing
  */
 void print_numbers(void)
 {
-int i;
-for (i = 48; i <= 57; i++)
-{
-_putchar(i);
+    int i;
+
+    for (i = 0; i <= 9; i++)
+        _putchar(i + '0');  /* 1re utilisation */
+
+    _putchar('\n');          /* 2e utilisation */
 }
-}
+
