04661b57a875bf95d554d412143040016dc0d708
poloelo <p.gioria2304@gmail.com>
2025-10-27 13:01:29 +0100

    Add the 8th prog

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_square(int size)
+{
+int x;
+int y;
+if (size <= 0) return (1); 
+
+for (x = 0; x < size; x++)
+    
+    for(y = 0; y < size; y++)
+    {
+        _putchar('#');
+    }
+
+    _putchar('\n');
+}
