33d6c42d9c353155d76000da7460c8e05c7ccfd7
poloelo <p.gioria2304@gmail.com>
2025-10-27 12:33:40 +0100

    Add the 6th prog

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_line - print a line with a lenght of n
+ * @n: lenght of the line
+ *
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i <= n; i++)
+_putchar('_');
+_putchar('\n');
+}
