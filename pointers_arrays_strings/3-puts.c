b0da997ad97318d81d50f9f83af21d45eef4fc1a
poloelo <p.gioria2304@gmail.com>
2025-10-29 14:59:38 +0100

    rectif

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,8 +1,12 @@
 #include "main.h"
-
+/**
+ * _puts - prints an array of chars followed by a new line
+ *
+ * @str: the pointer of the first character of the string
+ */
 void _puts(char *str)
 {
-while(*str != '\0')
+while (*str != '\0')
 {
 _putchar(*str);
 str++;
