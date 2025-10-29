ab543bef5059dfa24e2d615b3d50547280345bbf
poloelo <p.gioria2304@gmail.com>
2025-10-29 15:22:18 +0100

    rectif

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,33 @@
 #include "main.h"
 
-int _strlen(char *s);
-
-void print_rev(char *s)
-{
-char *c;
-c = s + _strlen(s) - 1;
-while(c >= s)
-{
-_putchar(*c);
-c--;
-}
-}
-
+/**
+ * _strlen - calcule la longueur d'une chaîne
+ * @s: chaîne de caractères
+ *
+ * Return: longueur de la chaîne
+ */
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (*s++)
-        len++;
-    return (len);
+	while (*s++)
+		len++;
+	return (len);
+}
+
+/**
+ * print_rev - imprime une chaîne en ordre inverse
+ * @s: chaîne à afficher
+ */
+void print_rev(char *s)
+{
+	char *c;
+
+	c = s + _strlen(s) - 1;
+	while (c >= s)
+	{
+		_putchar(*c);
+		c--;
+	}
+	_putchar('\n');
 }
