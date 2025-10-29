2546f8be04d600c33d0168640671d9284839e405
poloelo <p.gioria2304@gmail.com>
2025-10-29 13:16:25 +0100

    rectif

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * swap_int - Function that swaps the values of two variables
+ *
+ * @a: first variable changing
+ * @b: second variable changing
+ *
+ */
 void swap_int(int *a, int *b)
 {
 int n = *a;
