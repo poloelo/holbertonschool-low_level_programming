2407c973bba45200982de505a80711517c7f926f
poloelo <p.gioria2304@gmail.com>
2025-10-29 13:48:25 +0100

    rectif

--- pointers_arrays_strings/test.c
+++ pointers_arrays_strings/test.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
 void change(int *n);
-
+void swap_int (int *a, int *b);
 int main(void)
 {
 int table[5] = {1,2,3,4,5};
-int *p = table;
-printf("%p\n",p);
-printf("%d\n", *p);
-change(p);
-printf("%d", table[0]);
+int *a = table;
+int *b = table + 1;
+swap_int(a,b);
+printf("%d\n", table[0]);
+printf("%d\n", table[1]);
 }
 
 void change(int *n)
 {
 *n = 7;
+}
+ void swap_int(int *a, int *b)
+{
+int n = *a;
+*a = *b;
+*b = n;
 }
