daef8f7966985bc0f72cf2ee00a6caae3e5ec127
poloelo <p.gioria2304@gmail.com>
2025-10-29 13:36:49 +0100

    rectif

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,20 @@
 #include "main.h"
-
+/**
+ * _strlen - The strlen() function computes the length of the string s.
+ * The strnlen() function attempts to
+ * compute the length of s, but never
+ * scans beyond the first maxlen bytes of s.
+ *
+ * @s: the pointer of the first character of
+ * the string
+ *
+ * Return: Return the lenght of the string
+ */
 int _strlen(char *s)
 {
 int len = 0;
 
-while(*s != '\0')
+while (*s != '\0')
 {
 len++;
 s++;
