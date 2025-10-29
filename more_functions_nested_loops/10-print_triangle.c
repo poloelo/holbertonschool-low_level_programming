1100e4cbdcdcca2ade62ea4aa157c1a5aa02e836
poloelo <p.gioria2304@gmail.com>
2025-10-29 12:08:58 +0100

    rectif

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,6 +5,8 @@ void print_triangle(int size)
 int i;
 int j;
 int k;
+if (size > 0)
+{
 for(i = 0; i < size + 1; i++)
 {
 for (j = size - i; j > 0;j--) 
@@ -17,4 +19,6 @@ _putchar('#');
 }
 _putchar('\n');
 }
+}
+else _putchar('\n');
 }
