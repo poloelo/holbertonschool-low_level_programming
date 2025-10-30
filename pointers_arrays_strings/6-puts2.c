30b773969541e6e00ec1da9f116ebf6925e7b01b
poloelo <p.gioria2304@gmail.com>
2025-10-30 11:31:57 +0100

    rectif 6th

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,35 @@
 #include "main.h"
+/**
+ * _strlen - Fonction pour calculer le nombre de caractere d'un string
+ *
+ * @s: Le pointeur du premier element d'un tableau de char/ un string
+ *
+ * Return: int nombre de caractères
+ */
 
 int _strlen(char *s)
 {
-	int len = 0;
+int len = 0;
 
-	while (*s++)
-		len++;
-	return (len);
+while (*s++)
+len++;
+return (len);
 }
+
+/**
+* puts2 - Fonction qui print un caractere sur deux
+*
+* @str : //
+*
+*/
 
 void puts2(char *str)
 {
 int i;
 for (i = 0; i < _strlen(str); i++)
 {
-if (i == 0 || i%2 == 0) _putchar(str[i]);
+if (i == 0 || i % 2 == 0)
+_putchar(str[i]);
 }
 _putchar('\n');
 }
