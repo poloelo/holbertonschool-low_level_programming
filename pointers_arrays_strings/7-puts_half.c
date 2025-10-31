3ddb9b79fbdaea8ec5151555a2483d8cd9fb6705
poloelo <p.gioria2304@gmail.com>
2025-10-31 23:13:36 +0100

    rectif

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
+
+void puts_half(char *str)
+{
+int i;
+if (_strlen(str) % 2 == 0 ) i = _strlen(str) / 2 ;
+else i = (_strlen(str) / 2) + 1;
+
+for (i; i < _strlen(str); i++) _putchar(str[i]);
+}
