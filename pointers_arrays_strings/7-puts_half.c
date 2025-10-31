84910ae8d6fa2ec026291c7cda08b3d2e7a41c95
poloelo <p.gioria2304@gmail.com>
2025-10-31 23:28:57 +0100

    rectif

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,19 +1,36 @@
 #include "main.h"
 
+/**
+ * _strlen - calcule la longueur d'une chaîne
+ * @s: pointeur vers la chaîne de caractères
+ *
+ * Return: longueur de la chaîne
+ */
 int _strlen(char *s)
 {
 	int len = 0;
 
 	while (*s++)
 		len++;
+
 	return (len);
 }
 
+/**
+ * puts_half - affiche la deuxième moitié d'une chaîne
+ * @str: chaîne de caractères à afficher
+ */
 void puts_half(char *str)
 {
-int i;
-if (_strlen(str) % 2 == 0 ) i = _strlen(str) / 2 ;
-else i = (_strlen(str) / 2) + 1;
-for (; i < _strlen(str); i++) _putchar(str[i]);
-_putchar('\n');
+	int i;
+
+	if (_strlen(str) % 2 == 0)
+		i = _strlen(str) / 2;
+	else
+		i = (_strlen(str) / 2) + 1;
+
+	for (; i < _strlen(str); i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
 }
