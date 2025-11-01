6935534b15b615a3c9743a22c42d9014c1fd83fc
poloelo <p.gioria2304@gmail.com>
2025-11-01 02:41:24 +0100

    rectif

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,5 +4,12 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n; i++) printf("%d, ", a[i]);
+for (i = 0; i < n; i++) 
+{
+if (i != (n-1))
+{
+printf("%d, ", a[i]);
+}
+else 
+printf("%d\n", a[i]);
 }
