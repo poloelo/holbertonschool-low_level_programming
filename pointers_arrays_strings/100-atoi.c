b5aa0972eb1720d048f371bae051cf8828ed1bd2
poloelo <p.gioria2304@gmail.com>
2025-11-01 17:41:57 +0100

    rectif

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,30 +1,35 @@
 #include "main.h"
 
 /**
- * _atoi - convertit une chaîne de caractères en entier
+ * _atoi - convertit une chaîne en entier
  * @s: chaîne à convertir
  *
- * Return: l'entier converti, ou 0 si aucun chiffre trouvé
+ * Return: entier obtenu, ou 0 si aucun chiffre trouvé
  */
 int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
 	int number = 0;
-	int found = 0;
+	int found_digit = 0;
 
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
 			sign *= -1;
+
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
-			found = 1;
-			number = number * 10 + (s[i] - '0');
+			found_digit = 1;
+			/* on ajoute le nouveau chiffre à la fin du nombre */
+			number = (number * 10) + (s[i] - '0');
 		}
-		else if (found)
+
+		else if (found_digit)
 			break;
+
 		i++;
 	}
+
 	return (number * sign);
 }
