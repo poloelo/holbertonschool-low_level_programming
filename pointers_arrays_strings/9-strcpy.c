66903de2cc3a8a39c1b9942b80be283cc271131c
poloelo <p.gioria2304@gmail.com>
2025-11-01 03:25:43 +0100

    rectif

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,21 +1,36 @@
 #include "main.h"
 
+/**
+ * _strlen - calcule la longueur d'une chaîne de caractères
+ * @str: pointeur vers la chaîne
+ *
+ * Return: longueur de la chaîne
+ */
 int _strlen(char *str)
 {
-    int i = 0;
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-    return i;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
+/**
+ * _strcpy - copie une chaîne source vers une destination
+ * @dest: pointeur vers la destination
+ * @src: pointeur vers la source
+ *
+ * Return: pointeur vers dest
+ */
 char *_strcpy(char *dest, char *src)
 {
-    int i;
-    for (i = 0; i <= _strlen(src); i++)
-    {
-        dest[i] = src[i];
-    }
-   return dest;
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
