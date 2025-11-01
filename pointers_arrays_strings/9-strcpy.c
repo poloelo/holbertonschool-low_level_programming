82164d7b627af6847b353755c8e8705f17522ff9
poloelo <p.gioria2304@gmail.com>
2025-11-01 03:07:28 +0100

    rectif

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+int _strlen(char *str)
+{
+    int i = 0;
+    while (str[i] != "\0")
+    {
+        i++
+    }
+    return i;
+}
+
+char *_strcpy(char *dest, char *src)
+{
+    int i;
+    for (i = 0; i <= _strlen(src); i++)
+    {
+        *dest[i] = *src[i];
+    }
+   return dest;
+}
