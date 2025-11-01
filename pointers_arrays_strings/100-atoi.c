4459d856febce3bfd11f79e61994e0828ed87363
poloelo <p.gioria2304@gmail.com>
2025-11-01 17:54:51 +0100

    rectif

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,43 +1,32 @@
 #include "main.h"
 
 /**
- * _atoi - convertit une chaîne en entier
- * @s: chaîne à convertir
+ * _atoi - Convertit une chaîne de caractères en entier
+ * @s: Pointeur vers la chaîne à convertir
  *
- * Return: entier converti, ou 0 si aucun chiffre trouvé
+ * Return: L'entier converti depuis la chaîne
  */
 int _atoi(char *s)
 {
 	int i = 0;
 	int sign = 1;
-	int number = 0;
-	int found_digit = 0;
-	int digit;
+	long number = 0; /* utilisation de long pour éviter les overflow */
 
-	while (s[i] != '\0')
+	/* Ignorer les caractères non numériques et gérer les signes */
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
-		else if (s[i] >= '0' && s[i] <= '9')
-		{
-			found_digit = 1;
-			digit = s[i] - '0';
-
-			if (sign == 1 && number > (2147483647 - digit) / 10)
-				return (2147483647);
-			if (sign == -1 && number > (2147483648 - digit) / 10)
-				return (-2147483648);
-
-			number = (number * 10) + digit;
-		}
-		else if (found_digit)
-			break;
-
 		i++;
 	}
 
-	if (sign == -1)
-		number = -number;
+	/* Construire le nombre */
+	while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+	{
+		number = (number * 10) + (s[i] - '0');
+		i++;
+	}
 
-	return (number);
+	/* Appliquer le signe et retourner */
+	return ((int)(sign * number));
 }
