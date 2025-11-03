75de0ba641f3525d58ae7a9501a99bff5d0e04fd
poloelo <p.gioria2304@gmail.com>
2025-11-03 13:14:49 +0100

    rectif

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -5,7 +5,7 @@ char *_strstr(char *haystack, char *needle)
 {
     int i, j;
 
-    if (*needle == '\0') // cas spécial : needle vide
+    if (*needle == '\0')
         return (haystack);
 
     for (i = 0; haystack[i] != '\0'; i++)
@@ -16,7 +16,7 @@ char *_strstr(char *haystack, char *needle)
                 break;
         }
 
-        if (needle[j] == '\0') // si on a tout trouvé
+        if (needle[j] == '\0') 
             return (haystack + i);
     }
 
