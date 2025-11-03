b62bc533a1b02cbd3f3cc1ce8379a4d48152fac8
poloelo <p.gioria2304@gmail.com>
2025-11-03 10:22:07 +0100

    rectif

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,26 +1,44 @@
 #include "main.h"
 #include <stddef.h>
 
+/**
+ * _strlen - calcule la longueur d’une chaîne de caractères
+ * @str: la chaîne dont on veut connaître la longueur
+ *
+ * Return: la longueur de la chaîne
+ */
 int _strlen(char *str)
 {
-    int len = 0;
-    while(str[len] != '\0')
-    {
-        len++;
-    }
-    return len;
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	return (len);
 }
 
+/**
+ * _strchr - recherche la première occurrence d’un caractère dans une chaîne
+ * @s: la chaîne à parcourir
+ * @c: le caractère à rechercher
+ *
+ * Return: un pointeur vers la première occurrence de c dans s,
+ *         ou NULL si le caractère n’est pas trouvé
+ */
 char *_strchr(char *s, char c)
 {
-    int i;
-    int n = _strlen(s);
-    for (i = 0; i <= n; i++)
-    {
-        if (s[i] == c)
-        {
-            return (s + i);
-        }
-    }
-    return (NULL);
+	int i;
+	int n = _strlen(s);
+
+	for (i = 0; i <= n; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+
+	return (NULL);
 }
