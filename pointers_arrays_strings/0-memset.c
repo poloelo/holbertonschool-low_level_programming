f16849323f7616d6bb57039fa9dc107990dc37bf
poloelo <p.gioria2304@gmail.com>
2025-11-03 09:43:37 +0100

    rectif

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
+/**
+ * _memset - remplit une zone mémoire avec un octet constant
+ * @s: adresse du premier élément à remplacer
+ * @b: valeur à écrire dans chaque octet
+ * @n: nombre d’octets à remplir
+ *
+ * Return: pointeur vers la zone mémoire s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
-    for(i = 0; i < n; i++)
-    {
-    s[i] = b;
-    }
-return s;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
 }
+
