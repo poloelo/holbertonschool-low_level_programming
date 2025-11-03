4bd42bfed9044288d674baf17881b04adfd53dc5
poloelo <p.gioria2304@gmail.com>
2025-11-03 13:15:45 +0100

    rectif

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,25 +1,32 @@
 #include "main.h"
 #include <stddef.h>
 
+/**
+ * _strstr - Trouve la première occurrence d'une sous-chaîne dans une chaîne
+ * @haystack: la chaîne principale dans laquelle chercher
+ * @needle: la sous-chaîne à rechercher
+ *
+ * Return: pointeur vers le début de la sous-chaîne trouvée dans haystack,
+ *         ou NULL si la sous-chaîne n'est pas trouvée
+ */
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
+	int i, j;
 
-    if (*needle == '\0')
-        return (haystack);
+	if (*needle == '\0')
+		return (haystack);
 
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        for (j = 0; needle[j] != '\0'; j++)
-        {
-            if (haystack[i + j] != needle[j])
-                break;
-        }
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j])
+				break;
+		}
 
-        if (needle[j] == '\0') 
-            return (haystack + i);
-    }
+		if (needle[j] == '\0')
+			return (haystack + i);
+	}
 
-    return (NULL);
+	return (NULL);
 }
-
