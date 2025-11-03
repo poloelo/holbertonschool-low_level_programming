4d75b9fdf0b0f7e2a5c1072b54a27b89e5eac4a9
poloelo <p.gioria2304@gmail.com>
2025-11-03 09:36:47 +0100

    Add the 1st prog

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
