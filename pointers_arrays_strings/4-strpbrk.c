b8d0f9be8ea53285392e5cb5a12e31601f324666
poloelo <p.gioria2304@gmail.com>
2025-11-03 11:51:08 +0100

    rectif

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <std
+char *_strpbrk(char *s, char *accept)
+{
+    unsigned int i, j;
+
+    for (i = 0; i != '\0'; i++)
+    {
+        for (j = 0; j != '\0'; j++)
+        {
+            if (s[i] == s[j])
+                return (s + i);
+
+        }
+
+    }
+
+    return 
+
+}
