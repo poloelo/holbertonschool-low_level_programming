4aa2b0f71852ef7826e6b438a494c7015362362b
poloelo <p.gioria2304@gmail.com>
2025-11-05 13:41:43 +0100

    rectif

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,15 +1,21 @@
 #include "main.h"
 
+/**
+ * print_chessboard - Affiche un échiquier 8x8
+ * @a: tableau 2D contenant les pièces de l'échiquier
+ *
+ * Return: Rien (void)
+ */
 void print_chessboard(char (*a)[8])
 {
-    int i, j;
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            _putchar(a[i][j]);
-        }
+	int i, j;
 
-        _putchar('\n');
-    }
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
