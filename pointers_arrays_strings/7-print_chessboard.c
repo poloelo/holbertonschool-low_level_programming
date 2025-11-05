3aae9760c4eae85b1d3f31386c132caf6f7c5219
poloelo <p.gioria2304@gmail.com>
2025-11-05 13:21:27 +0100

    rectif

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -7,7 +7,7 @@ void print_chessboard(char (*a)[8])
     {
         for (j = 0; i < 8; j++)
         {
-            _putchar(a[i][j];
+            _putchar(a[i][j]);
         }
 
         _putchar('\n');
