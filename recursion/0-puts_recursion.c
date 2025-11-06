a690e264f85af7951d1d225422fd50ae7fbb06f2
poloelo <p.gioria2304@gmail.com>
2025-11-06 11:01:11 +0100

    rectif

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,14 +1,19 @@
 #include "main.h"
 
+/**
+ * _puts_recursion - imprime une chaîne de caractères suivie d’un saut de ligne
+ * @s: pointeur vers la chaîne à imprimer
+ *
+ * Return: rien (void)
+ */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-    }
-    else
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
