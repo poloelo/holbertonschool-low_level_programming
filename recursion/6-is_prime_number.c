56b4ba28b42caf929ff3d08d2fffc1cdd4c0e35f
poloelo <p.gioria2304@gmail.com>
2025-11-06 13:04:03 +0100

    rectif

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -11,10 +11,10 @@ int check_prime(int n, int divisor)
 {
 	if (divisor * divisor > n)
 		return (1);
-	
+
 	if (n % divisor == 0)
 		return (0);
-	
+
 	return (check_prime(n, divisor + 1));
 }
 
@@ -28,12 +28,12 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	
+
 	if (n == 2)
 		return (1);
-	
+
 	if (n % 2 == 0)
 		return (0);
-	
+
 	return (check_prime(n, 3));
 }
