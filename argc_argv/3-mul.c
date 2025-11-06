d80f68be76aaf49ff5bd35530ee54880dfb76f91
poloelo <p.gioria2304@gmail.com>
2025-11-06 14:23:14 +0100

    rectif

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,16 +11,13 @@
  */
 int main(int argc, char *argv[])
 {
-    int result;
-
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return (1);
-    }
-
-    result = atoi(argv[1]) * atoi(argv[2]);
-    printf("%d\n", result);
-
-    return (0);
+int result;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+result = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", result);
+return (0);
 }
