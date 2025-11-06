cd5b038a227aa52f9de12821d199881023ea3f8d
poloelo <p.gioria2304@gmail.com>
2025-11-06 13:54:54 +0100

    rectif

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,9 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - affiche tous les arguments passés au programme
+ * @argc: nombre d'arguments
+ * @argv: tableau des arguments
+ *
+ * Return: toujours 0
+ */
 int main(int argc, char *argv[])
 {
-int i; 
-for (i = 0; i < argc; i++) printf("%s\n", argv[i]);
-return (0);
+    int i;
+
+    for (i = 0; i < argc; i++)
+        printf("%s\n", argv[i]);
+
+    return (0);
 }
