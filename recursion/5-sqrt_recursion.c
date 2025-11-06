c8340b8fddcdc35bedd79db4514cde3ce5397630
poloelo <p.gioria2304@gmail.com>
2025-11-06 12:52:32 +0100

    rectif

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,8 +1,38 @@
 #include "main.h"
 
+/**
+ * sqrt_helper - Trouve la racine carrée en testant les valeurs
+ * @n: Le nombre dont on cherche la racine
+ * @guess: La valeur à tester
+ *
+ * Return: La racine carrée si elle existe, -1 sinon
+ */
+int sqrt_helper(int n, int guess)
+{
+	long square = (long)guess * guess;
+
+	if (square == n)
+		return (guess);
+	
+	if (square > n)
+		return (-1);
+	
+	return (sqrt_helper(n, guess + 1));
+}
+
+/**
+ * _sqrt_recursion - Calcule la racine carrée naturelle d'un nombre
+ * @n: Le nombre dont on cherche la racine carrée
+ *
+ * Return: La racine carrée naturelle, ou -1 si elle n'existe pas
+ */
 int _sqrt_recursion(int n)
 {
-    if (n < 0) return (-1);
-    if (n >= 0 && n <= 1) return 1;
-    return (_sqrt_recursion(n - 1) + n / _sqrt_recursion(n - 1)) / 2;
+	if (n < 0)
+		return (-1);
+	
+	if (n == 0 || n == 1)
+		return (n);
+	
+	return (sqrt_helper(n, 1));
 }
