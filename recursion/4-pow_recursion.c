fddd724516107a03f7a521321450622595cf885d
poloelo <p.gioria2304@gmail.com>
2025-11-06 12:27:13 +0100

    rectif

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
+/**
+ * _pow_recursion - calcule x à la
+ * puissance y de manière récursive
+ * 
+ * @x: base
+ * @y: exposant
+ *
+ * Return: le résultat de x^y, ou -1 si y est négatif
+ */
 int _pow_recursion(int x, int y)
 {
-    if (y < 0) return -1;
-    if (y >= 0 && y < 1) return 1;
+	if (y < 0)
+		return (-1);
 
-    return x * _pow_recursion(x, y - 1);
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
