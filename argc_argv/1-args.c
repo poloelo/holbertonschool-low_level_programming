6197ff654d42098b13da361834517b5acc3b9188
poloelo <p.gioria2304@gmail.com>
2025-11-06 13:43:43 +0100

    rectif

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,9 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - affiche le nombre d’arguments passés au programme
+ * @argc: nombre d’arguments
+ * @argv: tableau des arguments (non utilisé)
+ *
+ * Return: toujours 0
+ */
 int main(int argc, char *argv[])
 {
     (void)argv;
+
     printf("%d\n", argc);
+
     return (0);
 }
