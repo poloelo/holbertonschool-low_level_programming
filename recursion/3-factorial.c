a09777e145662b12fa84a4268eefc485e63b850e
poloelo <p.gioria2304@gmail.com>
2025-11-06 12:20:13 +0100

    rectif

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,9 +1,18 @@
 #include "main.h"
 
+/**
+ * factorial - calcule le facteur d’un nombre de façon récursive
+ * @n: nombre à calculer
+ *
+ * Return: le facteur de n, ou -1 si n est négatif
+ */
 int factorial(int n)
 {
-if (n < 0) return (-1);
-if (n <= 1 &&  n >= 0) return (1);
+	if (n < 0)
+		return (-1);
 
-return n * factorial(n - 1);
+	if (n == 0 || n == 1)
+		return (1);
+
+	return (n * factorial(n - 1));
 }
