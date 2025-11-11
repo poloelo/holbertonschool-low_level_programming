4ee2db76300bf62f7fdedc862d99bee73f7876ef
poloelo <p.gioria2304@gmail.com>
2025-11-11 21:52:35 +0100

    rectif

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,8 +12,8 @@ char *str_concat (char *s1, char *s2)
 {
     int i = 0;
     
-    if (s1 == NULL) s1 = '\0';
-    if (s2 == NULL) s2 = '\0';
+    if (s1 == NULL) s1 = "";
+    if (s2 == NULL) s2 = "";
 
     
     int len1 = _strlen(s1);
