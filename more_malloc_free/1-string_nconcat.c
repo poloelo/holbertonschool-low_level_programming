901ea88328ed898376eb349e2996f21d1156834c
poloelo <p.gioria2304@gmail.com>
2025-11-12 13:43:30 +0100

    rectif

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,22 +10,19 @@ int _strlen(char *str)
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int i; 
+    unsigned int i, len1, len2; 
     if (s1 == NULL) s1 = "";
     if (s2 == NULL) s2 = "";
 
     unsigned int len1 = _strlen(s1);
-    unsigned int len = _strlen(s1) + n + 1;
+    unsigned int len2 = _strlen(s2);
 
-    char *concat = malloc(((_strlen(s1) + n + 1))*sizeof(char));
+    char *concat = malloc((len1 + n + 1) * sizeof(char));
     if (concat == NULL) return NULL;
     
-    for (i = 0; i < len; i++)
-    {
-        if (i < len1 ) concat[i] = s1[i];
-        else if (i < len1 + n && i >= len1 ) concat[i] = s2[i - len1]; 
-    }
+    for (i = 0; i < len1; i++) concat[i] = s1[i];
+    for (i = 0; i < n; i++) concat[len1 + i] = s2[i];
     
-    concat[len] = '\0';
+    concat[len1 + n] = '\0';
     return concat; 
 }
