18dea734cae45635d51f4a84a768966f45d9c351
poloelo <p.gioria2304@gmail.com>
2025-11-13 14:00:51 +0100

    rectif

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,9 @@
+#include "dog.h"
+#include <stdio.h>
+
+void print_dog(struct dog *d)
+{
+    printf("Name: %s\n", d->name);
+    printf("Age: %f\n", d->age);
+    printf("Owner: %s\n", d->owner);
+}
