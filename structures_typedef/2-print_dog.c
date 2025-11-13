c5c92a619931fd17d1206a987a4f65979ac9ea04
poloelo <p.gioria2304@gmail.com>
2025-11-13 14:02:44 +0100

    rectif

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,9 +1,27 @@
 #include "dog.h"
 #include <stdio.h>
 
+/**
+ * print_dog - affiche les informations d'une structure dog
+ * @d: pointeur vers la structure à afficher
+ *
+ * Description: Si certains champs sont NULL, la fonction
+ * affiche "(nil)" à la place.
+ */
 void print_dog(struct dog *d)
 {
-    printf("Name: %s\n", d->name);
-    printf("Age: %f\n", d->age);
-    printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	printf("Age: %f\n", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
