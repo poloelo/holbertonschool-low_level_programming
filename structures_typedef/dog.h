f1f355062fc1b77fb7baf76943f70f7634b00095
poloelo <p.gioria2304@gmail.com>
2025-11-13 13:49:45 +0100

    rectif

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
+/**
+ * struct dog - structure contenant les informations d’un chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Description: Cette structure stocke les informations de base
+ * sur un chien : son nom, son âge et le nom de son propriétaire.
+ */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
