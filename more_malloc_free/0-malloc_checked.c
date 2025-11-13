0849851a3eba11504d40f6640169da8709a74e4d
poloelo <p.gioria2304@gmail.com>
2025-11-13 10:07:06 +0100

    rectif

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -9,10 +9,8 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);  /* on tente d’allouer b octets */
-
-    if (ptr == NULL)        /* si malloc a échoué */
-        exit(98);           /* on arrête le programme avec le code d’erreur 98 */
-
-    return ptr;             /* sinon on renvoie le pointeur vers la mémoire allouée */
+void *ptr = malloc(b);  /* on tente d’allouer b octets */
+if (ptr == NULL)        /* si malloc a échoué */
+exit(98);           /* on arrête le programme avec le code d’erreur 98 */
+return (ptr);
 }
