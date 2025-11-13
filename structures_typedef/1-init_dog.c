1c0b2427e2e19f578f8e15a3c771fb6c96aa9a07
poloelo <p.gioria2304@gmail.com>
2025-11-13 13:56:15 +0100

    rectif

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,9 +1,22 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * init_dog - initialise une variable de type struct dog
+ * @d: pointeur vers la structure à initialiser
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Description: Si le pointeur d n'est pas NULL, cette fonction
+ * initialise les champs de la structure dog avec les valeurs fournies.
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
