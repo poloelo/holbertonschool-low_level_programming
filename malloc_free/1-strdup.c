2982ab181c1d1bd3b1ab0fa3ab222e4394eccd6b
poloelo <p.gioria2304@gmail.com>
2025-11-13 10:46:51 +0100

    rectif

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,25 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _strlen - calcule la longueur d'une chaîne
+ * @str: chaîne de caractères
+ *
+ * Return: longueur de la chaîne
+ */
 int _strlen(char *str)
 {
-    int len = 0;
-    while(str[len] != '\0') 
-    {
-        len++;
-    }
-    return len;
+int len = 0;
+
+while (str[len] != '\0')
+len++;
+
+return (len);
 }
 
+/**
+ * _strdup - crée une copie d'une chaîne de caractères
+ * @str: chaîne à dupliquer
+ *
+ * Return: pointeur vers la copie, ou NULL si erreur
+ */
 char *_strdup(char *str)
 {
-    int i = 0;
-    int str_len = _strlen(str);
-    char* dup = malloc(str_len * sizeof(char));
-    
-    if (dup == NULL) return NULL;
-    else {
-        for(; i <= str_len; i++) dup[i] = str[i];
-        return dup;
-        }
+int i, str_len;
+char *dup;
+
+if (str == NULL)
+return (NULL);
+
+str_len = _strlen(str);
+dup = malloc((str_len + 1) * sizeof(char));
+if (dup == NULL)
+return (NULL);
+
+for (i = 0; i <= str_len; i++)
+dup[i] = str[i];
+
+return (dup);
 }
