687cf4aa7ffcc25a83baa68b436c2b0953298f32
poloelo <p.gioria2304@gmail.com>
2025-11-14 14:14:19 +0100

    recti

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,7 +1,17 @@
 #include "dog.h"
 #include <stdlib.h>
 
+/**
+ * free_dog - libère un dog_t
+ * @d: structure à libérer
+ *
+ * Description: Libère uniquement la struct si name et owner
+ * n'ont pas été alloués dynamiquement.
+ */
 void free_dog(dog_t *d)
 {
-    free(d);
+	if (d == NULL)
+		return;
+
+	free(d);
 }
