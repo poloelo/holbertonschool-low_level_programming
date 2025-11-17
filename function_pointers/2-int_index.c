77c6f021eb56e6539d8b7b9dadf3d65c3f2ede79
poloelo <p.gioria2304@gmail.com>
2025-11-17 11:20:08 +0100

    rectif

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+/**
+ * int_index - fonction qui va appliquer une
+ * verification sur un tableau de int
+ *
+ * @array: le tableau des valeurs qu'il faut verifier
+ * @size: taille du tableau (array)
+ * @cmp: pointeur vers une fonction de verification
+ * Return: -1 si erreur autre chiffre si bon
+ */
+#include "function_pointers.h"
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0)
+return (-1);
+
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) != 0)
+return (cmp(array[i]));
+}
+
+return (-1);
+}
