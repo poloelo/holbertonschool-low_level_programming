ac7cb99fdd690927bdaf89c4aae1b843ad6e7ce2
poloelo <p.gioria2304@gmail.com>
2025-11-17 10:39:03 +0100

    rectif

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,13 @@
+/**
+ * print_name - Fonction qui print un string en
+ * fonction d'une fonction/méthode demandé.
+ *
+ * @name: le nom a print
+ * @f: la methode qu'on veut appliquer au string
+ *
+ */
+#include "function_pointers.h"
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
-
+f(name);
 }
