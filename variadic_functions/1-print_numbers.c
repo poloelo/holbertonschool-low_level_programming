b8ebdc2586d4ad1e10712178487ebee0d28644fc
poloelo <p.gioria2304@gmail.com>
2025-11-20 12:35:35 +0100

    rectif

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,12 +1,14 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
     int i;
     va_list args;
     va_start(args, n);
+    if (separator == NULL) return;
     for (i = 0; i < n; i++)
     {
         if (i == n - 1) printf("%d\n", va_arg(args, int));
