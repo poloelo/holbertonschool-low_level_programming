8776469d4e6fa14bac08758be66b1f36158f85a4
poloelo <p.gioria2304@gmail.com>
2025-11-20 13:48:25 +0100

    rectif

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,32 +1,35 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
-#include <stdlib.h>
+#include <stdio.h>
 
-
+/**
+ * print_strings - Affiche des chaînes séparées par un séparateur
+ * @separator: Chaîne utilisée comme séparateur (peut être NULL)
+ * @n: Nombre d'arguments à afficher
+ *
+ * Return: Rien
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i; 
-    va_list args;
-    va_start(args, n);
-    
-    for (i = 0; i < n; i++)
-    {
-        char *c = va_arg(args, char *);
-        if (c != NULL)
-        printf("%s",c);
-        
-        else printf("nil");
+	unsigned int i;
+	char *s;
+	va_list args;
 
-        if (i < n - 1) 
-        {
-            if (separator == NULL)
-                continue;
-            else
-            printf ("%s", separator);
-        }
-    }    
-    printf("\n");
+	va_start(args, n);
 
-    va_end(args);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(args, char *);
+		if (s != NULL)
+			printf("%s", s);
+		else
+			printf("nil");
+
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(args);
 }
