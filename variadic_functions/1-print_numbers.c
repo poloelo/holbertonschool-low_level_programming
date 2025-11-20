f8a8e8026c3391b997bc772e7940be781cf79ad5
poloelo <p.gioria2304@gmail.com>
2025-11-20 12:38:45 +0100

    rectif

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,16 +3,29 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+/**
+ * print_numbers - Affiche des nombres séparés par un séparateur
+ * @separator: Chaîne utilisée pour séparer les nombres (peut être NULL)
+ * @n: Nombre d'arguments à afficher
+ *
+ * Return: Rien
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    int i;
-    va_list args;
-    va_start(args, n);
-    if (separator == NULL) return;
-    for (i = 0; i < n; i++)
-    {
-        if (i == n - 1) printf("%d\n", va_arg(args, int));
-        else printf("%d%s", va_arg(args, int),separator);
-    }
-    va_end(args);
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(args);
 }
