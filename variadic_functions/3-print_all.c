1f9eb96ea311e127791f2099eca7edabf2a6965d
poloelo <p.gioria2304@gmail.com>
2025-11-21 15:51:27 +0100

    rectif

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,98 +1,83 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-/* Déclaration du type de la structure pour le mapping */
-struct type_arg {
-    char c;
-    void (*type_f)(va_list);
-};
+/* --- Fonctions spécialisées --- */
 
-/* Déclarations des fonctions d'impression spécialisées */
-void print_int(va_list arg);
-void print_char(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-
-/* Tableau de mapping statique (dans la portée globale pour éviter le segfault sur la pile) */
-static struct type_arg types[] = {
-    { 'i', print_int },
-    { 'c', print_char },
-    { 'f', print_float },
-    { 's', print_string },
-    { 0, NULL } /* Marqueur de fin */
-};
-
-/* --- Fonctions d'impression spécialisées --- */
-
-void print_int(va_list arg)
+void p_char(va_list ap)
 {
-    printf("%i", va_arg(arg, int));
+	printf("%c", va_arg(ap, int));
 }
 
-void print_char(va_list arg)
+void p_int(va_list ap)
 {
-    /* char est promu à int */
-    printf("%c", va_arg(arg, int));
+	printf("%d", va_arg(ap, int));
 }
 
-void print_float(va_list arg)
+void p_float(va_list ap)
 {
-    /* float est promu à double */
-    printf("%f", va_arg(arg, double));
+	printf("%f", va_arg(ap, double));
 }
 
-void print_string(va_list arg)
+void p_string(va_list ap)
 {
-    char *s = va_arg(arg, char *);
-    
-    /* Le seul 'if' autorisé ici pour le cas NULL (contrainte) */
-    if (s == NULL)
-    {
-        printf("(nil)");
-        return;
-    }
-    printf("%s", s);
+	char *s = va_arg(ap, char *);
+	if (s == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", s);
 }
 
-/* --- Fonction principale print_all --- */
+/* --- Structure pour le mapping --- */
+typedef struct printer {
+	char c;
+	void (*f)(va_list);
+} printer_t;
+
+/* --- Fonction principale --- */
 
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    va_start(args, format);
-    
-    int i = 0;
-    int j;
-    
-    /* Boucle 1/2 : Parcourir la chaîne de format */
-    while (format && format[i] != '\0')
-    {
-        j = 0;
-        
-        /* Boucle 2/2 : Parcourir le tableau de types pour la correspondance */
-        while (types[j].c != 0)
-        {
-            /* IF 1/2 : Trouver le spécificateur */
-            if (format[i] == types[j].c)
-            {
-                types[j].type_f(args);
-                
-                /* IF 2/2 : Vérifier si ce n'est pas le dernier élément pour ajouter la virgule */
-                if (format[i + 1] != '\0')
-                {
-                    printf(", ");
-                }
-                
-                break; /* Sortir immédiatement de la boucle 'j' */
-            }
-            j++;
-        }
-        
-        i++;
-    }
-    
-    printf("\n");
-    va_end(args);
+	printer_t ops[] = {
+		{'c', p_char},
+		{'i', p_int},
+		{'f', p_float},
+		{'s', p_string},
+		{0, NULL}
+	};
+
+	va_list ap;
+	int i = 0, j, comma = 0;
+
+	va_start(ap, format);
+
+	/* WHILE n°1 : parcourir format */
+	while (format && format[i])
+	{
+		j = 0;
+
+		/* WHILE n°2 : parcourir les types */
+		while (ops[j].c)
+		{
+			/* IF n°1 : vérifier correspondance */
+			if (format[i] == ops[j].c)
+			{
+				/* IF n°2 : imprimer virgule si nécessaire */
+				if (comma)
+					printf(", ");
+
+				ops[j].f(ap);
+				comma = 1;
+
+				break;  /* pas d’else */
+			}
+			j++;
+		}
+		i++;
+	}
+
+	printf("\n");
+	va_end(ap);
 }
