e26136d99ca870d4712d56afe985bec07a1dac50
poloelo <p.gioria2304@gmail.com>
2025-12-04 10:56:25 +0100

    rectif the 0th prog

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,1 +1,15 @@
+#include "lists.h"
+
 size_t print_list(const list_t *h)
+{
+    int total = 0;
+    while(h->next != NULL)
+    {
+    printf("%s\n", h->str);
+    h = h->next;
+    total++;
+    }
+    printf("%s\n", h->str);
+    total++;
+    return total;
+}
