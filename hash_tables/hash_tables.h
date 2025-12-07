97f88e6220ef4679b76d72bbf4b8974e8e837204
poloelo <p.gioria2304@gmail.com>
2025-12-07 15:45:00 +0100

    hash tables

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -30,6 +30,9 @@ typedef struct hash_table_s
 } hash_table_t;
 
 /* PROTOTYPES */
+
+hash_table_t *hash_table_create(unsigned long int size);
+
 hash_table_t *hash_table_create(unsigned long int size);
 
 unsigned long int hash_djb2(const unsigned char *str);
